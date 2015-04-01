6b8c992ac2152c4a3bda424b5e171e159e3f13d7
Alexander Gabriel <alex@gabriel-software.ch>
2015-04-01 15:03:00 +0200

    oi_messages: remove doc after processing message

--- modules/handleChangesInMessageDb.js
+++ modules/handleChangesInMessageDb.js
@@ -2,6 +2,7 @@
  * when message doc changes
  * - analyse type
  * - do it
+ * - remove message doc
  */
 
 /*jslint node: true, browser: true, nomen: true, todo: true */
@@ -11,11 +12,18 @@ var nano            = require('nano')('http://barbalex:dLhdMg12@127.0.0.1:5984')
     createProjectDb = require('./createProjectDb');
 
 module.exports = function (change) {
-    var changeDoc = change.doc;
+    var changeDoc = change.doc,
+        oiDb      = nano.use('oi_messages');
 
     switch (changeDoc.type) {
     case 'projectAdd':
         createProjectDb(changeDoc.projectName);
+        // now delete the message
+        oiDb.destroy(changeDoc, changeDoc._rev, function (error) {
+            if (error) { return console.log('error removing doc after ordering to create project', error); }
+
+            console.log('removed doc after ordering to create project', changeDoc);
+        });
         break;
     }
 };
